a01342f8967d7e06f28c6a7d58c80de22009eba9
jkierzkowski <133491657+jkierzkowski@users.noreply.github.com>
2023-08-24 15:11:21 +0200

    Update RunDockerNeutrino.sh

--- RunDockerNeutrino.sh
+++ RunDockerNeutrino.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker run --name geniecontainer --rm -e DISPLAY=$DISPLAY -v $GENIE_IM_WDIR:/opt/Home -p 8888:8888 jankierzkowski/neutrino_genie
+sudo docker run --name geniecontainer --rm -e DISPLAY=$DISPLAY -v $PWD:/opt/Home -p 8888:8888 jankierzkowski/neutrino_genie
 exit
